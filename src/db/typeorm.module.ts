73530e6dedeb0e9a4de78317eee1b6ea7a7d0928
afdavletkulov <afdavletkulov@rttv.ru>
2023-09-14 18:12:51 -0300

    fix-docker-file

--- src/db/typeorm.module.ts
+++ src/db/typeorm.module.ts
@@ -6,11 +6,11 @@ console.log(process.env.POSTGRES_DATABASE);
   imports: [
     NestTypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USERNAME,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      host: process.env.POSTGRES_HOST, //localhost
+      port: Number(process.env.POSTGRES_PORT), //5432
+      username: process.env.POSTGRES_USERNAME, //test ??
+      password: process.env.POSTGRES_PASSWORD, //test ??
+      database: process.env.POSTGRES_DATABASE, //accountant ??
       entities: ['dist/entities/**/*.entity.js'],
       synchronize: true,
       // migrations: [ 'dist/db/migrations/**/*.js' ],
