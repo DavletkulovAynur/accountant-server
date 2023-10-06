4eefa42ea5f04579c333d0e69e9608d9ec062735
afdavletkulov <afdavletkulov@rttv.ru>
2023-10-06 18:55:27 -0300

    delete put

--- src/entities/accountant/accountant.service.ts
+++ src/entities/accountant/accountant.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Accountant } from './accountant.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { OPERATION_TYPE } from './types';
 
 @Injectable()
 export class AccountantService {
@@ -14,7 +15,29 @@ export class AccountantService {
     return await this.accountantRepository.save(newOperation);
   }
 
-  public async getAllExpenceOperations() {
+  public async getAllOperations() {
     return await this.accountantRepository.find();
+  }
+
+  public async getAllExpenceOperations() {
+    return await this.findTransactions(OPERATION_TYPE.Expence);
+  }
+
+  public async getAllIncomeOperations() {
+    return await this.findTransactions(OPERATION_TYPE.Income);
+  }
+
+  public async deleteOperation() {
+    // return await this.findTransactions(OPERATION_TYPE.Income);
+  }
+
+  public async editOperation() {
+    // return await this.findTransactions(OPERATION_TYPE.Income);
+  }
+
+  async findTransactions(type: OPERATION_TYPE): Promise<Accountant[]> {
+    return this.accountantRepository.find({
+      where: { operationType: type },
+    });
   }
 }
